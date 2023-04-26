6a74491bbc6556b819d8370a6dd0a6fcd2820138
User <96388713+aura80@users.noreply.github.com>
2023-04-26 23:53:48 +0300

    Try with resources added

---
+++ exceptions/TryWithResources.java
@@ -0,0 +1,25 @@
+package java_exercises_github.exceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TryWithResources {
+    public static void main(String[] args) {
+        File file = new File("fisier.txt");
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
