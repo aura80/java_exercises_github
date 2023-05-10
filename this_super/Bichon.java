0c26fbbd851ce389c10bfda9920bb6f3fcbc661c
User <96388713+aura80@users.noreply.github.com>
2023-05-10 23:38:25 +0300

    this super example added

--- this_super/Bichon.java
+++ this_super/Bichon.java
@@ -13,8 +13,11 @@ public class Bichon {
     }
 
     public Bichon(String name, int age) {
-        this.name = name;
-        this.age = age;
+        this.name = name;                   // this is referring to the name field of the class/of the object of the Bichon class type
+        this.age = age;                     // this is referring to the age field of the class/of the object of the Bichon class type
     }
 
+    public String getName() {
+        return this.name;       // this is used here as a good practice only because name from the getName() method is the same as the name field of the class
+    }
 }
